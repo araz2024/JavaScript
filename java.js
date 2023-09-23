e9e169e6d43bec86a26c585a427b4a5ebc1dd5b2
witcher2022 <araz.ahmadov2017@gmail.com>
2023-09-23 16:47:02 +0400

    dərslik

---
+++ java.js
@@ -0,0 +1,12 @@
+// let araz = 1;
+// for (let i=0; i<1100; i++) {
+//     console.log(i);
+// }
+
+//Yadda saxla tez tez bax bu kodlara ki unutmayasan
+
+// let araz = 1;
+// while (araz<1000){
+//     console.log(araz);
+//     araz++;
+// }
