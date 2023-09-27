98b5a26e899e232db941afcbab3c216e3c600a0b
witcher2022 <araz.ahmadov2017@gmail.com>
2023-09-27 22:16:07 +0400

    OOP-nəzəriyyəsi

---
+++ OOP.js
@@ -0,0 +1,32 @@
+//Standart obyektlərin yaranması 
+// let resume = {
+//     name: "Araz",
+//     sureName: "Ahmadov",
+//     age: 30,
+//     country: "Azerbaijan",
+//     city: "Ganja",
+//     phone: "+9945548884**",
+//     getName: function(){
+//     return this.name;
+//     }
+// };
+//     console.log(resume);
+
+
+// Burda isə müəllim local function yaratmışam və içinə parameter qoymuşam və bildirmişəmki adı və yaşı geri qaytar qısa desək şablon yaratmışam
+    // function createNewPerson(_name,_age){
+    //     return {
+    //         name:_name,
+    //         age:_age,
+    //         getName:function(){
+    //             return this.name;
+    //         }
+    //     }
+    // };
+    
+    // let person01 = createNewPerson("John",20);
+    // console.log(person01);
+    
+
+    
+
