131d068d2ba770aaff64a0ecee943c7de3decc8a
witcher2022 <araz.ahmadov2017@gmail.com>
2023-10-19 20:17:11 +0400

    tez tez bax

--- OOP.js
+++ OOP.js
@@ -29,16 +29,64 @@
 
 
 
-    class resume {
-        constructor(name,sureName, age,country,city,phone){
-            this.name = name;
-            this.sureName =sureName;
-            this.age = age;
-            this.country = country;
-            this.city = city;
-            this.phone = phone;
-        }
-    };
+    // class resume {
+    //     constructor(name,sureName, age,country,city,phone){
+    //         this.name = name;
+    //         this.sureName =sureName;
+    //         this.age = age;
+    //         this.country = country;
+    //         this.city = city;
+    //         this.phone = phone;
+    //     }
+    // };
     
-    let myResume = new resume ("Araz","Ahmadov",30,"Azerbaijan","Ganja","+9945548884**",);
-    console.log(myResume);
+    // let myResume = new resume ("Araz","Ahmadov",30,"Azerbaijan","Ganja","+9945548884**",);
+    // console.log(myResume);
+
+
+    // let myResume02 = new resume ("Orxan","Ahmadaov","Azerbaijan",36,"Ganja","+99450*****",);
+    // console.log(myResume02);
+
+
+    //Function-nan istifade qaydasi
+
+    // let a = 100;
+// let b = 50;
+
+// function sum (a, b) {
+//     if (a > b){
+//         return true;
+    
+//     }else if((a < b)){
+    
+//         return false;
+//     }
+// };
+
+// let toplama = sum(500,450);
+// console.log(toplama);
+
+// function metninIlkHerfi (metin){
+//     console.log(metin[0]);
+// }
+// metninIlkHerfi('salam')
+
+// function basHerfiTap(metin){
+//     let index = metin.length - 4
+//     console.log(metin[index])
+// };
+
+// basHerfiTap("Salam necesen");
+
+
+// function herfinSonu (metin){
+//     let herfSayi = 0;
+//     for (let i = 0; i < metin.length; i++){
+//         if (metin[i]=='a'){
+//             herfSayi++
+//         }
+//     } console.log(herfSayi);
+// };
+// herfinSonu('salam aleykum natarsan'); 
+
+
